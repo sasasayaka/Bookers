b280001d80e52c55c2ceb26e5977726477cd8f6c
sasasayaka <130236704+sasasayaka@users.noreply.github.com>
2023-07-17 12:19:21 +0000

    [update] bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
   get '/top' => 'homes#top'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
 end
